4dc0ec14177e9535f02a159693c848457662a516
josearenasmanrique <112592865+josearenasmanrique@users.noreply.github.com>
2023-09-12 10:16:49 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página PHP de Ejemplo</title>
+</head>
+<body>
+    <h1>Bienvenido a mi página PHP</h1>
+    
+    <p>La fecha y hora actual es: <?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
